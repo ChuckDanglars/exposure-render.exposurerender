10c13604d84cc644f4304d20bc5ea26fc269ac33
thomaskroes <thomaskroes@localhost>
2013-11-01 16:09:19 +0100

    Work on socket data

---
+++ utilities/network/camerasocketdata.cpp
@@ -0,0 +1,68 @@
+
+#include "camerasocketdata.h"
+
+#include <QFile>
+#include <QFileInfo>
+
+QCameraSocketData::QCameraSocketData(QObject* Parent /*= 0*/) :
+	QSocketData(Parent),
+	Camera()
+{
+}
+
+QCameraSocketData::~QCameraSocketData()
+{
+}
+
+void QCameraSocketData::Receive(QByteArray& Data)
+{
+	
+	
+	QDataStream DataStream(&Data, QIODevice::ReadOnly);
+	DataStream.setVersion(QDataStream::Qt_4_0);
+
+	QByteArray Voxels;
+
+	DataStream.writeBytes((char*)&this->Camera, sizeof(this->Camera));
+
+	/*
+	DataStream >> this->FileName;
+		
+	DataStream >> this->Resolution[0];
+	DataStream >> this->Resolution[1];
+	DataStream >> this->Resolution[2];
+
+	DataStream >> this->Spacing[0];
+	DataStream >> this->Spacing[1];
+	DataStream >> this->Spacing[2];
+		
+	DataStream >> Voxels;
+	DataStream >> this->NoBytes;
+
+	memcpy(this->Voxels, Voxels.data(), this->NoBytes);
+	*/
+}
+
+void QCameraSocketData::Send(QBaseSocket* Socket, QByteArray& Data)
+{
+	QByteArray ByteArray;
+	QDataStream DataStream(&ByteArray, QIODevice::WriteOnly);
+	DataStream.setVersion(QDataStream::Qt_4_0);
+
+	/*
+	DataStream << FileInfo.fileName();
+		
+	DataStream << this->Resolution[0];
+	DataStream << this->Resolution[1];
+	DataStream << this->Resolution[2];
+
+	DataStream << this->Spacing[0];
+	DataStream << this->Spacing[1];
+	DataStream << this->Spacing[2];
+		
+	DataStream << Voxels;
+	DataStream << this->NoBytes;
+	*/
+
+	QSocketData::Send(Socket, ByteArray);
+}
---
+++ utilities/network/camerasocketdata.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "socketdata.h"
+#include "api\camera.h"
+
+#include <QString>
+
+using namespace ExposureRender;
+
+class QCameraSocketData : public QSocketData
+{
+    Q_OBJECT
+
+public:
+    QCameraSocketData(QObject* Parent = 0);
+	virtual ~QCameraSocketData();
+
+	virtual void Receive(QByteArray& Data);
+	virtual void Send(QBaseSocket* Socket, QByteArray& Data);
+
+private:
+	Camera		Camera;
+};
